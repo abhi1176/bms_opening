7dc6fab3ce5d9ce72761ae8bf61c9e99e80fe061
Abhishek Chauhan Korra <abhishek.k@avenirdigital.ai>
2019-11-14 14:47:43 +0530

    scrape.py: Bug fix

--- scrape.py
+++ scrape.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 from mailsender import SendEmail
 
 SUBJECT = "Movie tickets are open"
-BODY = "BMS bookings for theater: \"{}\" is open for your movie on BookMyShow @ {}."
+BODY = "BMS bookings for theaters: \"{}\" is open for your movie on BookMyShow @ {}."
 
 EMAIL_ADDRESS = os.environ.get("EMAIL_USER")
 EMAIL_PASSWORD = os.environ.get("EMAIL_PASS")
@@ -28,21 +28,21 @@ def search_venue(link, venues, to_address):
 		except:
 			pass
 
-	venues = [venue.lower() for venue in venues]
-	found_venues = []
+	found_venues = [venue
+		for bms_venue in soup.find_all('a', class_="__venue-name")
+			for venue in venues
+				if venue.strip().lower() in bms_venue.text.strip().lower()]
 
-	for bms_venue in soup.find_all('a', class_="__venue-name"):
-		for venue in venues:
-			if venue.strip().lower() in bms_venue.text.strip().lower():
-				found_venues.append(venue)
-		if found_venues:
-			se = SendEmail(subject=SUBJECT,
-						   body=BODY.format(", ".join(found_venues), link),
-						   from_address=EMAIL_ADDRESS,
-						   password=EMAIL_PASSWORD,
-						   to_address=to_address)
-			se.send_email()
-			print("Email sent successfully to {}".format(to_address))
+	print("Venues found: {}".format(found_venues))
+	
+	if found_venues:
+		se = SendEmail(subject=SUBJECT,
+					   body=BODY.format(", ".join(found_venues), link),
+					   from_address=EMAIL_ADDRESS,
+					   password=EMAIL_PASSWORD,
+					   to_address=to_address)
+		se.send_email()
+		print("Email sent successfully to {}".format(to_address))
 
 
 if __name__ == "__main__":
@@ -55,4 +55,4 @@ if __name__ == "__main__":
 	args = parser.parse_args()
 	search_venue(args.bms_link, args.venues, args.to)
 
-# python scrape.py -v "AMB Cinemas" -l "https://in.bookmyshow.com/buytickets/doctor-sleep-hyderabad/movie-hyd-ET00104940-MT/20191114"
+# python scrape.py -v "AMB Cinemas" "preston prime" "inorbit" -l "https://in.bookmyshow.com/buytickets/khaidi-hyderabad/movie-hyd-ET00114552-MT/20191114"
